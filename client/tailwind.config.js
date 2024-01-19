b29dd95d326526e60b12763381d837ffd8238f36
Zalerys <gustaveconstans@gmail.com>
2024-01-19 17:55:17 +0100

    fix(main): setup talwind class text

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,43 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontSize: {
+      xl: [
+        "30px",
+        {
+          lineHeight: "36px",
+          letterSpacing: "-0.01em",
+          fontWeight: "700",
+        },
+      ],
+
+      lg: [
+        "18px",
+        {
+          lineHeight: "28px",
+          letterSpacing: "-0.01em",
+          fontWeight: "700",
+        },
+      ],
+
+      base: [
+        "16px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "-0.01em",
+          fontWeight: "400",
+        },
+      ],
+
+      sm: [
+        "14px",
+        {
+          lineHeight: "20px",
+          letterSpacing: "-0.01em",
+          fontWeight: "400",
+        },
+      ],
+    },
   },
   plugins: [],
-}
+};
