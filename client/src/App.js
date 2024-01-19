6d6f7702dc0b3aa23a9608d4e528798741a77fee
Zalerys <gustaveconstans@gmail.com>
2024-01-19 16:34:56 +0100

    fix(main): chakra setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import Restaurants from "./pages/Restaurants";
 import Menus from "./pages/Menus";
 import Orders from "./pages/Orders";
@@ -8,16 +10,18 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Restaurants />} />
-          <Route path="/*" element={<Restaurants />} />
-          <Route path="/Restaurants" element={<Restaurants />} />
-          <Route path="/Restaurants/Menus" element={<Menus />} />
-          <Route path="/Orders" element={<Orders />} />
-          <Route path="/Connexion" element={<Connexion />} />
-        </Routes>
-      </BrowserRouter>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Restaurants />} />
+            <Route path="/*" element={<Restaurants />} />
+            <Route path="/Restaurants" element={<Restaurants />} />
+            <Route path="/Restaurants/Menus" element={<Menus />} />
+            <Route path="/Orders" element={<Orders />} />
+            <Route path="/Connexion" element={<Connexion />} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 }
