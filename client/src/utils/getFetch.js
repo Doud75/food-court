72892bcd20ee332a1c004ea0e72ec18ab926a0d7
Zalerys <gustaveconstans@gmail.com>
2024-01-19 11:25:58 +0100

    feat(fetch): add post + get fetch in utils

---
+++ client/src/utils/getFetch.js
@@ -0,0 +1,12 @@
+export async function getFetch(url) {
+  try {
+    const response = await fetch(`http://localhost:8097${url}`);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
