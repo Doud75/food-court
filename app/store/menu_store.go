387caf6a0285bdd98cffd084b4ff704128328914
M3dzZ <mehdichaibi05@gmail.com>
2024-01-21 17:05:25 +0100

    fix: get menus by id_restaurant

--- app/store/menu_store.go
+++ app/store/menu_store.go
@@ -19,26 +19,26 @@ type MenuStore struct {
 func (r *MenuStore) GetMenuByRestaurantID(restaurantID string) ([]facade.MenuItem, error) {
 	var menus []facade.MenuItem
 
-	rows, err := r.Query(`SELECT id, dishes, price, restaurant_id FROM "menu"`)
+	query := `SELECT id, dishes, price, restaurant_id FROM "menu" WHERE restaurant_id = $1`
+	rows, err := r.Query(query, restaurantID)
 	if err != nil {
 		fmt.Println(err)
-		return []facade.MenuItem{}, err
+		return nil, err
 	}
-
 	defer rows.Close()
 
 	for rows.Next() {
 		var menu facade.MenuItem
 		if err = rows.Scan(&menu.ID, &menu.Dishes, &menu.Price, &menu.RestaurantID); err != nil {
 			fmt.Println(err)
-			return []facade.MenuItem{}, err
+			return nil, err
 		}
 		menus = append(menus, menu)
 	}
 
 	if err = rows.Err(); err != nil {
 		fmt.Println(err)
-		return []facade.MenuItem{}, err
+		return nil, err
 	}
 
 	return menus, nil
