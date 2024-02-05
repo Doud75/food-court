7d4a920342a388d6cf68202afd4c6c8b24fdc8c4
Zalerys <gustaveconstans@gmail.com>
2024-02-05 22:30:31 +0100

    fix(order) add name restaurant

--- app/facade/order.go
+++ app/facade/order.go
@@ -15,6 +15,17 @@ type OrderItem struct {
 	RestaurantID  uuid.NullUUID   `json:"restaurant_id"`
 }
 
+type OrderItemWithRestaurant struct {
+    ID            uuid.NullUUID   `json:"id"`
+    DishesList    json.RawMessage `json:"dishes_list"`
+    TotalPrice    float64         `json:"total_price"`
+    Reference     int64           `json:"reference"`
+    State         string          `json:"state"`
+    UserID        uuid.NullUUID   `json:"user_id"`
+    RestaurantID  uuid.NullUUID   `json:"restaurant_id"`
+    RestaurantName string         `json:"restaurant_name"`
+}
+
 type OrderStoreInterface interface {
-	GetOrdersByUser(uuid.UUID) ([]OrderItem, error)
+	GetOrdersByUser(uuid.UUID) ([]OrderItemWithRestaurant, error)
 }
