cdc9c0cdafcb0593f6657a3bda694a2ae3b2abba
DimitriChauvel <chauvel.dimitri@posteo.net>
2024-02-07 08:02:18 +0100

    wip

--- app/go.mod
+++ app/go.mod
@@ -4,13 +4,10 @@ go 1.21.6
 
 require (
 	github.com/go-chi/chi v1.5.5
+	github.com/golang-jwt/jwt/v5 v5.2.0
+	github.com/google/uuid v1.5.0
 	github.com/lib/pq v1.10.9
+	github.com/rs/cors v1.10.1
 	golang.org/x/crypto v0.18.0
+	golang.org/x/net v0.10.0
 )
-
-require (
-	github.com/google/uuid v1.5.0 // indirect
-	github.com/rs/cors v1.10.1 // indirect
-  github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
-)
-
--- app/main.go
+++ app/main.go
@@ -5,12 +5,53 @@ import (
 	"fmt"
 	"food_court/controller"
 	"food_court/store"
-	_ "github.com/lib/pq"
+	"io"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
+	"golang.org/x/net/websocket"
 )
 
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connexion from client", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		fmt.Println(string(msg))
+		ws.Write([]byte("thank you for the message."))
+	}
+}
+
 func main() {
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
 	connStr := "user=user dbname=FOOD password=password port=5454 sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
