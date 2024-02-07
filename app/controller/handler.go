fac01ab4b0b999ce102abfeff688d89aa329e44e
DimitriChauvel <chauvel.dimitri@posteo.net>
2024-02-07 09:14:08 +0100

    wip

--- app/controller/handler.go
+++ app/controller/handler.go
@@ -1,11 +1,55 @@
 package controller
 
 import (
+	"fmt"
 	"food_court/store"
+	"io"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/rs/cors"
+
+	"golang.org/x/net/websocket"
 )
+
+func NewWebsocketHandler() *WebsocketHandler {
+	return &WebsocketHandler{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (h *WebsocketHandler) HandleWS(ws *websocket.Conn) {
+	h.conns[ws] = true
+
+	h.ReadLoop(ws)
+}
+
+func (h *WebsocketHandler) ReadLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		h.Broadcast(msg)
+	}
+}
+
+func (h *WebsocketHandler) Broadcast(b []byte) {
+	for ws := range h.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("write error:", err)
+			}
+		}(ws)
+	}
+}
 
 func NewHandler(store *store.Store) *Handler {
 	handler := &Handler{
@@ -13,13 +57,16 @@ func NewHandler(store *store.Store) *Handler {
 		store,
 	}
 
+	userver := NewWebsocketHandler()
+	rserver := NewWebsocketHandler()
+	
 	corsOptions := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"Content-Type"},
 		AllowCredentials: true,
 	})
-
+	
 	handler.Use(corsOptions.Handler)
 	handler.Use(middleware.Logger)
 	/*User*/
@@ -39,11 +86,16 @@ func NewHandler(store *store.Store) *Handler {
 	handler.Get("/orders-client/{userID}", handler.GetOrdersByUser())
 	handler.Get("/orders-restaurant/{restaurantID}", handler.GetPendingOrdersByRestaurant())
 	handler.Post("/orders/{restaurantID}/{orderID}", handler.UpdateOrderToDone())
-
+	handler.Handle("/user", websocket.Handler(userver.HandleWS))
+	handler.Handle("/restaurant", websocket.Handler(rserver.HandleWS))
 	return handler
 }
 
 type Handler struct {
 	*chi.Mux
 	*store.Store
+}
+
+type WebsocketHandler struct {
+	conns map[*websocket.Conn]bool
 }
